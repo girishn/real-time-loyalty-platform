132689ac2aaadd213683a59ba6827209272af3f3
girishn <n_girish@hotmail.com>
2025-08-13 20:27:54 +1000

    Refactoring terraform to modular structure

--- infra/terraform/ec2_client/main.tf
+++ infra/terraform/ec2_client/main.tf
@@ -42,3 +42,48 @@ resource "aws_instance" "kafka_client" {
     Environment = var.environment
   }
 }
+
+resource "aws_key_pair" "msk_key" {
+  key_name   = "msk-key"
+  public_key = file("~/.ssh/msk-key.pub")
+}
+
+resource "aws_iam_role_policy" "ec2_scram_policy" {
+  role = aws_iam_role.ec2_kafka_iam_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ]
+        Resource = aws_secretsmanager_secret.msk_scram_secret.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "ec2_kafka_iam_role" {
+  name = "ec2_kafka_iam_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_kafka_profile"
+  role = aws_iam_role.ec2_kafka_iam_role.name
+}
+
+
+# Data sources for dynamic values
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
---
+++ infra/terraform/ec2_client/variables.tf
@@ -0,0 +1,18 @@
+variable "environment" {
+  description = "Deployment environment (e.g., dev, prod)"
+  type        = string
+  default     = "dev"
+  
+}
+
+variable "kafka_client_ami" {
+  description = "AMI ID for the Kafka client EC2 instance"
+  type        = string
+  default     = "ami-0de716d6197524dd9"
+}
+
+variable "kafka_client_instance_type" {
+  description = "Instance type for the Kafka client EC2 instance"
+  type        = string
+  default     = "t3.micro"
+}
